e1dfd012b91f986bf9f1988c5ada52564bf643e7
sebastiantorres86 <sebastian.torres.mail@gmail.com>
2021-01-25 21:50:41 -0300

    new version of the awesome slider

--- src/components/CarouselContainer/CarouselContainer.js
+++ src/components/CarouselContainer/CarouselContainer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container } from "./Carousel.styled";
-import Carousel from "@brainhubeu/react-carousel";
-import "@brainhubeu/react-carousel/lib/style.css";
+import AwesomeSlider from "react-awesome-slider";
+import "react-awesome-slider/dist/styles.css";
 import dashboard1 from "../../assets/dashboard1.jpg";
 import dashboard2 from "../../assets/dashboard2.jpg";
 import dashboard3 from "../../assets/dashboard3.png";
@@ -11,13 +11,13 @@ import dashboard5 from "../../assets/dashboard5.jpg";
 const CarouselContainer = () => {
   return (
     <Container>
-      <Carousel plugins={["arrows", "infinite"]}>
-        <img src={dashboard1} alt="dashboard1" />
-        <img src={dashboard2} alt="dashboard2" />
-        <img src={dashboard3} alt="dashboard3" />
-        <img src={dashboard4} alt="dashboard4" />
-        <img src={dashboard5} alt="dashboard5" />
-      </Carousel>
+      <AwesomeSlider>
+        <div data-src={dashboard1} />
+        <div data-src={dashboard2} />
+        <div data-src={dashboard3} />
+        <div data-src={dashboard4} />
+        <div data-src={dashboard5} />
+      </AwesomeSlider>
     </Container>
   );
 };
